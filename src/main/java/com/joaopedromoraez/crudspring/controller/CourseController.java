aeac2e437dc1665186ac1ee25785f79abdbddb69
João Pedro Moraes <joaopedromoraes@outlook.com>
2023-04-19 22:51:47 -0300

    feat: create post course

--- src/main/java/com/joaopedromoraez/crudspring/controller/CourseController.java
+++ src/main/java/com/joaopedromoraez/crudspring/controller/CourseController.java
@@ -1,11 +1,10 @@
 package com.joaopedromoraez.crudspring.controller;
+
 import com.joaopedromoraez.crudspring.model.Course;
 import com.joaopedromoraez.crudspring.repository.CourseRepository;
-
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,11 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 }
